337c56d1ff8bc12ac50bb2c989b8ef5e82e005e9
Marek Szwałkiewicz <marek@szwalkiewicz.waw.pl>
2014-12-10 18:54:51 +0100

    Added status checking (pull) handle.

--- mprofi_api_client/connector.py
+++ mprofi_api_client/connector.py
@@ -11,6 +11,7 @@ class MprofiAPIConnector(object):
     api_version = '1.0'
     send_endpoint = 'send'
     sendbulk_endpoint = 'sendbulk'
+    status_endpoint = 'status'
 
     def __init__(self, api_token=None, payload=None):
         self.token = api_token or os.environ.get('MPROFI_API_TOKEN', '')
@@ -19,6 +20,7 @@ class MprofiAPIConnector(object):
             'Authorization': 'Token {0}'.format(self.token)
         })
         self.payload = payload or []
+        self.response = []
 
     def add_message(self, recipient, message):
 
@@ -41,6 +43,7 @@ class MprofiAPIConnector(object):
                 full_payload.update({
                     'reference': reference
                 })
+            extract_from_response = lambda r: [{'id': r['id']}]
 
         elif len(self.payload) > 1:
             used_endpoint = self.sendbulk_endpoint
@@ -51,6 +54,7 @@ class MprofiAPIConnector(object):
                 full_payload.update({
                     'reference': reference
                 })
+            extract_from_response = lambda r: r['result']
 
         else:
             raise ValueError("Empty payload. Please use `add_message` first.")
@@ -63,4 +67,41 @@ class MprofiAPIConnector(object):
 
         encoded_payload = json.dumps(full_payload)
 
-        self.session.post(full_url, json=encoded_payload, verify=True)
+        response = self.session.post(
+            full_url,
+            json=encoded_payload,
+            verify=True
+        )
+
+        response_json = response.json()
+        self.response = self.payload
+        self.payload = []
+
+        for sent_message, response_message in zip(
+                    self.response,
+                    extract_from_response(response_json)
+        ):
+            sent_message.update(response_message)
+
+        return response_json
+
+    def get_status(self):
+
+        status_full_url = '/'.join([
+            self.url_base,
+            self.api_version,
+            self.status_endpoint, ""
+        ])
+
+        for sent_message in self.response:
+            message_id = sent_message['id']
+
+            response = self.session.get(
+                status_full_url,
+                params={'id': message_id},
+                verify=True
+            )
+
+            sent_message.update(response.json())
+
+        return self.response
