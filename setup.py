05a2652d5d842f1fad52600cd92a4134c99cadb4
Marek Szwałkiewicz <marek.szwalkiewicz@materna.com.pl>
2015-06-09 11:35:02 +0200

    Fixed packaging

--- setup.py
+++ setup.py
@@ -12,17 +12,29 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
+packages = [
+    'mprofi_api_client',
+    'mprofi_api_client.packages',
+    'mprofi_api_client.packages.requests',
+    'mprofi_api_client.packages.requests.packages',
+    'mprofi_api_client.packages.requests.packages.chardet',
+    'mprofi_api_client.packages.requests.packages.urllib3',
+    'mprofi_api_client.packages.requests.packages.urllib3.packages',
+    'mprofi_api_client.packages.requests.packages.urllib3.contrib',
+    'mprofi_api_client.packages.requests.packages.urllib3.util',
+    'mprofi_api_client.packages.requests.packages.urllib3.packages.ssl_match_hostname',
+]
+
+
 setup(
     name='mprofi_api_client',
-    version='0.1.1',
+    version='0.1.3',
     description='Python Client library for mProfi REST API',
     long_description=readme + '\n\n' + history,
     author='Materna Communications Sp. z o.o.',
     author_email='pomoc@materna.com.pl',
     url='https://github.com/materna/mprofi_api_client_python',
-    packages=[
-        'mprofi_api_client',
-    ],
+    packages=packages,
     package_dir={'mprofi_api_client':
                  'mprofi_api_client'},
     include_package_data=True,
